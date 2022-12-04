48d400fbc992ab342d76d192c64a066381bbc337
Phillippe Santana <phillippelevidad@gmail.com>
2022-12-04 12:10:29 -0300

    Ability to make the guarded value optional

--- src/Guard.ts
+++ src/Guard.ts
@@ -1,8 +1,22 @@
 import { GuardError } from "./GuardError";
 
 export class Guard {
+  private _isOptional = false;
+
   constructor(public value: any, public parameterName = "value") {
     if (value === undefined) this.value = null;
+  }
+
+  /**
+   * Ensures that the value is a valid JavaScript number.
+   * @param message Optional message. If not provided, a default message will be used.
+   * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
+   */
+  array(message?: string): Guard {
+    return this.ensure(
+      (value) => Array.isArray(value),
+      message ?? `${this.parameterName} '${this.value} must be a number.`
+    );
   }
 
   /**
@@ -39,22 +53,22 @@ export class Guard {
    * @param callback A function that will validate or act upon each item. This function can expect to receive a @see Guard object containing the current value.
    * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
    */
-  each(callback: (guard: Guard) => void): Guard {
+  each(callback: (guard: Guard, keyOrIndex: string | number) => void): Guard {
     if (typeof this.value === "object") {
       for (const key of Object.keys(this.value))
-        callback(new Guard(this.value[key], this.parameterName));
+        callback(this.makeNewGuard(this.value[key]), key);
       return this;
     }
     if (Array.isArray(this.value)) {
-      for (const item of this.value)
-        callback(new Guard(item, this.parameterName));
+      for (let i = 0; i < this.value.length; i++)
+        callback(this.makeNewGuard(this.value[i]), i);
       return this;
     }
     return this;
   }
 
   /**
-   * Ensures that the value, if present, exactly equals (===) the specified value.
+   * Ensures that the value exactly equals (===) the specified value.
    * @param value The value to compare the guarded value against.
    * @param message Optional message. If not provided, a default message will be used.
    * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
@@ -67,7 +81,7 @@ export class Guard {
   }
 
   /**
-   * Ensures that the value, if present, is a valid email address.
+   * Ensures that the value is a valid email address.
    * @param message Optional message. If not provided, a default message will be used.
    * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
    */
@@ -80,11 +94,12 @@ export class Guard {
   }
 
   /**
-   * If a value is present, 'ensure' runs it through the provided function and expects it to return true in order to be considered valid.
+   * Runs the guarded value through the provided functionm which is expected to return true in order for the guarded value to be considered valid.
    * @param message Optional message. If not provided, a default message will be used.
    * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
    */
   ensure(isValid: (value: any) => boolean, message?: string): Guard {
+    this.throwIfNotOptionalAndNoValue();
     if (this.hasValue() && isValid(this.value) !== true)
       throw new GuardError(
         this.parameterName,
@@ -94,7 +109,7 @@ export class Guard {
   }
 
   /**
-   * Ensures that the value, if present, is a valid hostname (domain name).
+   * Ensures that the value is a valid hostname (domain name).
    * @param message Optional message. If not provided, a default message will be used.
    * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
    */
@@ -106,7 +121,7 @@ export class Guard {
   }
 
   /**
-   * Ensures that the value, if present, matches one of the possible values.
+   * Ensures that the value matches one of the possible values.
    * @param possibleValues An array of the possible values.
    * @param message Optional message. If not provided, a default message will be used.
    * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
@@ -122,7 +137,7 @@ export class Guard {
   }
 
   /**
-   * Ensures that the value, if present, is a valid instance of the specified class.
+   * Ensures that the guarded value is a valid instance of the specified class.
    * @param classType The class prototype (just pass in the class itself).
    * @param message Optional message. If not provided, a default message will be used.
    * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
@@ -136,7 +151,7 @@ export class Guard {
   }
 
   /**
-   * Ensures that the value, if present, is in the valid integer.
+   * Ensures that the value is in the valid integer.
    * @param message Optional message. If not provided, a default message will be used.
    * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
    */
@@ -149,7 +164,7 @@ export class Guard {
   }
 
   /**
-   * Ensures that the value, if present, is a valid ISO datetime string.
+   * Ensures that the value is a valid ISO datetime string.
    * @param message Optional message. If not provided, a default message will be used.
    * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
    */
@@ -161,7 +176,7 @@ export class Guard {
   }
 
   /**
-   * Ensures that the value, if present, is a string or array,
+   * Ensures that the value is a string or array,
    * has length lesser than or equal to @argument max,
    * and greater than or equal to @argument min.
    * @param length The min length allowed for the guarded value.
@@ -173,26 +188,22 @@ export class Guard {
   }
 
   /**
-   * If the value is present and is an Array, ensures that its entries are unique.
-   * @param keySelector A function to extract a key from an element.
+   * If the value is present and is an Array, makes its elements unique.
+   * @param keySelector A function to extract a key from an element. If not provided, the element itself will be used as the key.
    * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
    */
-  makeUniqueBy<TItem = any>(keySelector: (item: TItem) => any): Guard {
+  makeUnique<TItem = any>(keySelector?: (item: TItem) => any): Guard {
     if (Array.isArray(this.value)) {
-      const mapItems = new Map(
-        this.value.map((item) => {
-          const key = keySelector(item);
-          return [key, item];
-        })
+      const uniqueValues = new Set(
+        this.value.map(keySelector ?? ((item) => item))
       );
-      const uniqueItems = [...mapItems.values()];
-      return guard(uniqueItems, this.parameterName);
+      return this.makeNewGuard(uniqueValues);
     }
     return this;
   }
 
   /**
-   * Ensures that the value, if present, is a number lesser than or equal to @argument maxValue.
+   * Ensures that the value is a number lesser than or equal to @argument maxValue.
    * @param maxValue The max number value allowed for the guarded value.
    * @param message Optional message. If not provided, a default message will be used.
    * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
@@ -206,7 +217,7 @@ export class Guard {
   }
 
   /**
-   * Ensures that the value, if present, is a string or array with length lesser than or equal to @argument length.
+   * Ensures that the value is a string or array with length lesser than or equal to @argument length.
    * @param length The min length allowed for the guarded value.
    * @param message Optional message. If not provided, a default message will be used.
    * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
@@ -221,7 +232,7 @@ export class Guard {
   }
 
   /**
-   * Ensures that the value, if present, is a number greater than or equal to @argument minValue.
+   * Ensures that the value is a number greater than or equal to @argument minValue.
    * @param minValue The min number value allowed for the guarded value.
    * @param message Optional message. If not provided, a default message will be used.
    * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
@@ -235,7 +246,7 @@ export class Guard {
   }
 
   /**
-   * Ensures that the value, if present, is a string or array with length greater than or equal to @argument length.
+   * Ensures that the value is a string or array with length greater than or equal to @argument length.
    * @param length The min length allowed for the guarded value.
    * @param message Optional message. If not provided, a default message will be used.
    * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
@@ -250,7 +261,7 @@ export class Guard {
   }
 
   /**
-   * Ensures that the value, if present, does NOT exactly equals (!==) the specified value.
+   * Ensures that the value, does NOT exactly equals (!==) the specified value.
    * @param value The value to compare the guarded value against.
    * @param message Optional message. If not provided, a default message will be used.
    * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
@@ -263,7 +274,7 @@ export class Guard {
   }
 
   /**
-   * Ensures that the value, if present, is not one of the specified values.
+   * Ensures that the value is not one of the specified values.
    * @param possibleValues An array of the unwanted values.
    * @param message Optional message. If not provided, a default message will be used.
    * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
@@ -279,35 +290,20 @@ export class Guard {
   }
 
   /**
-   * Ensures that the value has value, that is, it is not null or undefined.
+   * Ensures that the value is not empty.
+   * If the value is a string, it must have at least one character.
+   * If the value is an array, it must have at least one element.
+   * If the value is an object, it must have at least one property.
    * @param message Optional message. If not provided, a default message will be used.
    * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
    */
-  notNull(message?: string): Guard {
-    if (!this.hasValue())
-      throw new GuardError(
-        this.parameterName,
-        message ?? `${this.parameterName} must not be null.`
-      );
-    return this;
-  }
-
-  /**
-   * Ensures that the value has a value (not null nor undefined) and that it is not empty, which is a check
-   * that works against objects (must have at least one key), arrays (at least one element) and strings
-   * (length greater then 0).
-   * @param message Optional message. If not provided, a default message will be used.
-   * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
-   */
-  notNullOrEmpty(message?: string): Guard {
-    if (this.hasValue()) return this;
-    if (typeof this.value === "object" && Object.keys(this).length) return this;
-    if (typeof this.value === "string" && this.value.length > 0) return this;
-    if (Array.isArray(this.value) && this.value.length > 0) return this;
-    throw new GuardError(
-      this.parameterName,
-      message ?? `${this.parameterName} must not be null or empty.`
-    );
+  notEmpty(message?: string): Guard {
+    return this.ensure((val) => {
+      if (typeof val === "string") return val.length > 0;
+      if (Array.isArray(val)) return val.length > 0;
+      if (typeof val === "object") return Object.keys(val).length > 0;
+      return true;
+    }, message ?? `${this.parameterName} must not be empty.`);
   }
 
   /**
@@ -315,15 +311,15 @@ export class Guard {
    * @param message Optional message. If not provided, a default message will be used.
    * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
    */
-  notNullOrWhitespace(message?: string): Guard {
-    guard(this.value?.trim(), this.parameterName).notNullOrEmpty(
+  notEmptyOrWhitespace(message?: string): Guard {
+    guard(this.value?.trim(), this.parameterName).notEmpty(
       message ?? `${this.parameterName} must not be empty.`
     );
     return this;
   }
 
   /**
-   * Ensures that the value, if present, is a valid JavaScript number.
+   * Ensures that the value is a valid JavaScript number.
    * @param message Optional message. If not provided, a default message will be used.
    * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
    */
@@ -335,7 +331,29 @@ export class Guard {
   }
 
   /**
-   * Ensures that the guarded value, if present, is a string and matches the provided regex pattern.
+   * Ensures that the value is a valid JavaScript object.
+   * @param message Optional message. If not provided, a default message will be used.
+   * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
+   */
+  object(message?: string): Guard {
+    return this.ensure(
+      (value) => typeof value === "object",
+      message ?? `${this.parameterName} '${this.value} must be a number.`
+    );
+  }
+
+  /**
+   * Marks the guarded value as optional,
+   * so that guard functions will not throw an error if the value is null or undefined.
+   * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
+   */
+  optional(): Guard {
+    this._isOptional = true;
+    return this;
+  }
+
+  /**
+   * Ensures that the guarded value is a string and matches the provided regex pattern.
    * @param regex The regex pattern.
    * @param message Optional message. If not provided, a default message will be used.
    * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
@@ -348,7 +366,7 @@ export class Guard {
   }
 
   /**
-   * Ensures that the guarded value, if present, is a string and matches at least one of the provided patterns.
+   * Ensures that the guarded value is a string and matches at least one of the provided patterns.
    * @param regexes An array of regexes.
    * @param message Optional message. If not provided, a default message will be used.
    * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
@@ -362,7 +380,7 @@ export class Guard {
   }
 
   /**
-   * Ensures that the value, if present and is a number, is in the specified range.
+   * Ensures that the value and is a number, is in the specified range.
    * @param maxValue The max number value allowed for the guarded value.
    * @param message Optional message. If not provided, a default message will be used.
    * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
@@ -375,14 +393,14 @@ export class Guard {
   }
 
   /**
-   * Ensures that the value, if present, is a valid URL.
+   * Ensures that the value is a valid JavaScript string.
    * @param message Optional message. If not provided, a default message will be used.
    * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
    */
-  url(message?: string): Guard {
-    return this.pattern(
-      /^(ftp|http|https):\/\/[^ "]+$/,
-      message ?? `${this.parameterName} must be a valid URL.`
+  string(message?: string): Guard {
+    return this.ensure(
+      (value) => typeof value === "string",
+      message ?? `${this.parameterName} '${this.value} must be a string.`
     );
   }
 
@@ -425,11 +443,51 @@ export class Guard {
   }
 
   /**
+   * Ensures that the value is an array and that all of its entries are unique.
+   * @param keySelector A function to extract a key from an element. If not provided, the element itself will be used as the key.
+   * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
+   */
+  unique<TItem = any>(keySelector?: (item: TItem) => any): Guard {
+    return this.ensure((val) => {
+      if (!Array.isArray(val)) return false;
+      const uniqueValues = new Set(val.map(keySelector ?? ((item) => item)));
+      return uniqueValues.size === val.length;
+    }, `${this.parameterName} must be an array with unique entries.`);
+  }
+
+  /**
+   * Ensures that the value is a valid URL.
+   * @param message Optional message. If not provided, a default message will be used.
+   * @returns A @see Guard object, for following up with other guard methods or obtaining the input value.
+   */
+  url(message?: string): Guard {
+    return this.pattern(
+      /^(ftp|http|https):\/\/[^ "]+$/,
+      message ?? `${this.parameterName} must be a valid URL.`
+    );
+  }
+
+  /**
    * Returns a value indicating whether the guarded value is null or undefined.
    * @returns true if the guarded value is not null or undefined. Otherwise, returns false.
    */
   hasValue(): boolean {
     return this.value !== null && this.value !== undefined;
+  }
+
+  private makeNewGuard(value: any): Guard {
+    const g = guard(value, this.parameterName);
+    if (this._isOptional) g.optional();
+    return g;
+  }
+
+  private throwIfNotOptionalAndNoValue(): void | never {
+    if (this._isOptional) return;
+    if (this.hasValue()) return;
+    throw new GuardError(
+      this.parameterName,
+      `${this.parameterName} is required.`
+    );
   }
 }
 
